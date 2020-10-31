052f226b2bcc2ea2d17ccb87f132c61eff9c6b67
Atle Magnussen <atlemagnussen@gmail.com>
2020-10-31 18:34:06 +0100

    added headers

--- test/server-static.js
+++ test/server-static.js
@@ -5,22 +5,27 @@ const fs = require("fs");
 const key  = fs.readFileSync('./ssl_certs/server.key');
 const cert = fs.readFileSync('./ssl_certs/server.crt');
 const ca   = fs.readFileSync('./ssl_certs/server.csr');
+const port = 3000;
 
-const options = {
-    key,
-    cert
-};
+console.log("start");
+const server = createQuicSocket({ endpoint: { port } });
+server.listen({ key, cert, alpn: 'hello' });
 
-const server = createQuicSocket({ port: 3000 });
-server.listen(options);
+console.log("now session handler");
 server.on("session", session => {
-    session.on("stream", (stream, headers) => {
-        if (headers[":path"] === "/") 
+    console.log("on session");
+    session.on("stream", (stream) => {
+        console.log("on stream");
+        stream('initialHeaders', (headers) => {
+            console.log("on headers");
+            console.log(headers);
+            if (headers[":path"] === "/") 
             stream.respondWithFile("./files/index.html");
-        else {
-            const re = /\/(\w+)*/;
-            const filename = headers[":path"].replace(re, "$1");
-            stream.respondWithFile(`./files/${filename}`);
-        }
+            else {
+                const re = /\/(\w+)*/;
+                const filename = headers[":path"].replace(re, "$1");
+                stream.respondWithFile(`./files/${filename}`);
+            }
+        });
     });
 });
