b0e5a683cc9e949ee4d205e8f807beadb5c94006
Atle Magnussen <atlemagnussen@gmail.com>
2020-10-31 15:21:10 +0100

    now works

--- test/server.js
+++ test/server.js
@@ -8,6 +8,8 @@ const cert = fs.readFileSync('./ssl_certs/server.crt');
 const ca   = fs.readFileSync('./ssl_certs/server.csr');
 const port = 1234;
 
+console.log("start");
+
 // Create the QUIC UDP IPv4 socket bound to local IP port 1234
 const server = createQuicSocket({ endpoint: { port } });
 
@@ -15,6 +17,8 @@ const server = createQuicSocket({ endpoint: { port } });
 // key and certificate to secure new connections, using
 // the fictional 'hello' application protocol.
 server.listen({ key, cert, alpn: 'hello' });
+
+console.log("server listen");
 
 server.on('session', (session) => {
   // The peer opened a new stream!
@@ -41,20 +45,29 @@ const socket = createQuicSocket({
   }
 });
 
-const req = socket.connect({
-  address: 'localhost',
-  port,
-});
+(async function() {
+    console.log("try connect client");
+    const client = await socket.connect({
+        address: 'localhost',
+        port,
+    });
+    client.on('secure', async () => {
+        console.log("client secure");
+        
+        const str = await client.openStream();
+        
+        process.stdin.pipe(str);
+        
+        str.on('data', (chunk) => {
+            console.log(`client(on-secure): ${chunk.toString()}`);
+        });
+        str.on('end', () => {
+            console.log('client(on-secure): end');
+        });
+        str.on('close', () => {
+            // Graceful shutdown
+            socket.close();
+        });
+    });
+})();
 
-req.on('secure', () => {
-  const stream = req.openStream();
-  // stdin -> stream
-  process.stdin.pipe(stream);
-  stream.on('data', (chunk) => console.log('client(on-secure): ', chunk.toString()));
-  stream.on('end', () => console.log('client(on-secure): end'));
-  stream.on('close', () => {
-    // Graceful shutdown
-    socket.close();
-  });
-  stream.on('error', (err) => console.error(err));
-});
