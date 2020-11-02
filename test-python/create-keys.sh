28f8283891fbc16d71a249dbbd276a1ef21c82bb
Atle Magnussen <atlemagnussen@gmail.com>
2020-11-02 08:53:18 +0100

    one line it

--- test-python/create-keys.sh
+++ test-python/create-keys.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-openssl req -newkey rsa:2048 -nodes -keyout certificate.key \
-                   -x509 -out certificate.pem -subj '/CN=Test Certificate' \
-                   -addext "subjectAltName = DNS:localhost"
+openssl req -newkey rsa:2048 -nodes -keyout certificate.key -x509 -out certificate.pem -subj '/CN=Test Certificate' -addext "subjectAltName = DNS:localhost"
 
